8b228de28afc5dd000fc51dad19d29ee55e1aa7a
JIHOON LEE <eziya76@gmail.com>
2022-07-09 22:54:35 +0900

    TrustZone test
    TrustZone test

---
+++ TEST_PROJECTS/STM32_HAL_TRUSTZONE/NonSecure/Debug/objects.mk
@@ -0,0 +1,9 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+USER_OBJS := D:/Workspace/STM32/TEST/STM32_HAL_TRUSTZONE/Secure/Debug/secure_nsclib.o
+
+LIBS :=
+
