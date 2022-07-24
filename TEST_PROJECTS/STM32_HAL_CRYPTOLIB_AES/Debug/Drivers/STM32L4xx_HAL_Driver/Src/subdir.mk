fbd3d0a3348ea67282dc0caf8fd26b9f6da3e03c
JIHOON LEE <eziya76@gmail.com>
2022-07-24 22:34:04 +0900

    update ECDH
    update ECDH

--- TEST_PROJECTS/STM32_HAL_CRYPTOLIB_AES/Debug/Drivers/STM32L4xx_HAL_Driver/Src/subdir.mk
+++ TEST_PROJECTS/STM32_HAL_CRYPTOLIB_AES/Debug/Drivers/STM32L4xx_HAL_Driver/Src/subdir.mk
@@ -70,7 +70,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Drivers/STM32L4xx_HAL_Driver/Src/%.o Drivers/STM32L4xx_HAL_Driver/Src/%.su: ../Drivers/STM32L4xx_HAL_Driver/Src/%.c Drivers/STM32L4xx_HAL_Driver/Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L476xx -c -I"E:/Workspace/STM32_SECURITY_LAB/TEST_PROJECTS/STM32_HAL_CRYPTOLIB_AES/Middlewares/ST/STM32_Cryptographic/include" -I../Core/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L476xx -c -I"D:/Workspace/STM32/STM32_SECURITY_LAB/TEST_PROJECTS/STM32_HAL_CRYPTOLIB_AES/Middlewares/ST/STM32_Cryptographic/include" -I../Core/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Drivers-2f-STM32L4xx_HAL_Driver-2f-Src
 
