23b3d413fea12f2d1e5fac68ab65b3b0c8238239
JIHOON LEE <eziya76@gmail.com>
2023-01-17 22:21:39 +0900

    remove LFS big files
    remove

--- MOOC_DOCS/Part6/STM32CubeExpansion_SBSFU_V2.6.1/Projects/B-L4S5I-IOT01A/Applications/2_Images_STSAFE/2_Images_SECoreBin/Binary/Certs/CHECK_CERTIFICATE_CHAINS_OEM1.bat
+++
@@ -1,3 +0,0 @@
-echo "Check OEM1 chain..."
-C:\local\Tools\OpenSSL-Win64\bin\openssl verify -verbose -show_chain -trusted sbsfu_r0_crt.pem -untrusted sbsfu_inter1_oem1_crt.pem -untrusted sbsfu_inter2_oem1_crt.pem sbsfu_fwsign_oem1_crt.pem
-pause
